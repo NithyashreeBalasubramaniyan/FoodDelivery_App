5c648e7b3d5814701806fbac80ad8ed4af46482d
NithyashreeBalasubramaniyan <balanithyashree2004@gmail.com>
2025-07-08 11:24:38 +0530

    Update StoreContext.jsx

--- frontend/src/StoreContext.jsx
+++ frontend/src/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext=createContext(null)
 const StoreContextProvider=(props) =>{
 
 
-    const url='https://fooddelivery-app-k8kc.onrender.com'
+    const url='https://fooddelivery-app-k8kc.onrender.com' 
     const [food_list,setFood_list]=useState([])
     const [cartItem,setCartItem]=useState({})
     const [token,setToken]=useState("")
