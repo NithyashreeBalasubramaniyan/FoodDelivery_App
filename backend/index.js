5479337af0a4b2e5711d9ff238ce9d3690ca46df
NithyashreeBalasubramaniyan <balanithyashree2004@gmail.com>
2025-07-08 11:27:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,11 @@ const port=process.env.PORT||4000
 //middleware   
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: 'https://fooddelivery-app-frontend-ax6p.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
 
 connectDB()
 
